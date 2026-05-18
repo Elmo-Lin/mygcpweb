952009a66eb0a19852708211e470e82860f87417
Elmo-Lin <alvin4883889@gmail.com>
2026-05-18 21:55:20 +0800

    Init: Init the project.

---
+++ src/main/java/com/example/mygcpweb/MygcpwebApplication.java
@@ -0,0 +1,13 @@
+package com.example.mygcpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygcpwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MygcpwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mygcpweb/MygcpwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mygcpweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MygcpwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
